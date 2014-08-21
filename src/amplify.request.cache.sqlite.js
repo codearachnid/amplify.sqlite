eaaeb3d9114c3e0452c846ee027a0c3994a6cb10
Timothy Wood <tim@imaginesimplicity.com>
2014-08-21 13:04:56 -0400

    Update amplify.request.cache.sqlite.js

--- src/amplify.request.cache.sqlite.js
+++ src/amplify.request.cache.sqlite.js
@@ -1,50 +1,41 @@
 // define custom caching approach for amplify when data is requested
-amplify.request.cache.sqlite = function( resource, request, ajax, ampXHR ){
-
-
-    // console.log('---------------------');
-    // console.log("resource",resource);
-    // console.log("request",request);
-    // console.log("ajax",ajax);
-    // console.log("ampXHR",ampXHR);
-    // console.log('---------------------');
-
-    
-    var cacheKey = amplify.request.cache._key( request.resourceId, ajax.cacheURL(), ajax.data );
-
-	if(  typeof request == 'object' && request.hasOwnProperty('getRemote') && request.getRemote == true ) {
-
+amplify.request.cache.sqlite = function(resource, request, ajax, ampXHR) {
+	// console.log('---------------------');
+	// console.log("resource",resource);
+	// console.log("request",request);
+	// console.log("ajax",ajax);
+	// console.log("ampXHR",ampXHR);
+	// console.log('---------------------');
+	var cacheKey = amplify.request.cache._key(request.resourceId, ajax.cacheURL(),
+		ajax.data);
+	if (typeof request == 'object' && request.hasOwnProperty('getRemote') &&
+		request.getRemote == true) {
 		var ampXHRsuccess = ampXHR.success;
-		ampXHR.success = function( data, status ) {
-
-	        var expiration = typeof resource.cache == 'object' ? Number( resource.cache.expires ) + Date.now() : 0; 
-	        amplify.sqlite.instance.put( cacheKey, data, expiration ).done(function(){
-				if ( typeof resource.cache == 'object' && typeof resource.cache.expires == "number" && resource.cache.expires > 0 ) {
-					setTimeout(function() {
-						amplify.sqlite.instance.delete( cacheKey );
-					}, Number( resource.cache.expires ) );
-				}
-	        });
-	        ampXHRsuccess.apply( this, arguments );
-
-	    };
-
+		ampXHR.success = function(data, status) {
+			var expiration = typeof resource.cache == 'object' ? Number(
+				resource.cache.expires) + Date.now() : 0;
+			amplify.sqlite.instance.put(cacheKey, data, expiration).done(
+				function() {
+					if (typeof resource.cache == 'object' && typeof resource
+						.cache.expires == "number" && resource.cache
+						.expires > 0) {
+						setTimeout(function() {
+							amplify.sqlite.instance.delete(
+								cacheKey);
+						}, Number(resource.cache.expires));
+					}
+				});
+			ampXHRsuccess.apply(this, arguments);
+		};
 	} else {
-
-		amplify.sqlite.instance.get( cacheKey ).done(function( dataFromCache ){
-
-			if( typeof dataFromCache == 'object' )
-				dataFromCache.fromCache = true;
-
-	        ampXHR.success( dataFromCache );
-
-		}).fail(function(error){
-
+		amplify.sqlite.instance.get(cacheKey).done(function(dataFromCache) {
+			if (typeof dataFromCache == 'object') dataFromCache.fromCache =
+				true;
+			ampXHR.success(dataFromCache);
+		}).fail(function(error) {
 			request.getRemote = true;
 			amplify.request(request);
-
 		});
 		return false;
 	}
-
 };
